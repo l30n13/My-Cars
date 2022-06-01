f138b2e360e015ad421554a339e1daba01d28a7f
Leon <mrtonmoy12@gmail.com>
2022-06-01 21:34:19 +0600

    -- request manager updated

--- Cars/Helpers/Managers/RequestManger.swift
+++ Cars/Helpers/Managers/RequestManger.swift
@@ -7,48 +7,67 @@
 
 import Foundation
 import Alamofire
-import NotificationBannerSwift
 
 struct RequestManager {
     enum RequestType: String, CaseIterable {
         case get    = "GET"
         case post   = "POST"
     }
+    enum ErrorType: Error {
+        case noInternet
+        case networkProblem
+        case errorDescription(Error)
+        
+    }
     
-    typealias response = ((String) -> Void)
+    typealias successResponse = ((Data) -> Void)
+    typealias errorResponse = ((ErrorType) -> Void)
     
-    func request(using url: String,
+    static func request(using url: HttpURL,
                  params: [String : AnyObject]?,
                  type: RequestType,
                  header: HTTPHeaders? = nil,
-                 success: @escaping response,
-                 failure: @escaping response) {
+                 success: @escaping successResponse,
+                 failure: @escaping errorResponse) {
         
         DLog("API URL: \(url)\nHeader data: \(String(describing: header))")
         NetworkManager.isReachable { (reachable) in
             if reachable {
-                AF.request(url, method: HTTPMethod(rawValue: type.rawValue), parameters: params, encoding: JSONEncoding.default, headers: header).responseString(encoding: String.Encoding.utf8) { (responseString) -> Void in
-                    switch responseString.response?.statusCode {
+                AF.request(url.url, method: HTTPMethod(rawValue: type.rawValue), parameters: params, encoding: JSONEncoding.default, headers: header).responseData { (responseData) -> Void in
+                    switch responseData.response?.statusCode {
                     case 200, 201:
-                        switch responseString.result {
-                        case .success(let responseString):
-                            success(responseString)
+                        switch responseData.result {
+                        case .success(let responseData):
+                            success(responseData)
                         case .failure(let error):
-                            failure(error.localizedDescription)
+                            failure(.errorDescription(error))
                         }
                         break
                     case 400, 401, 404, 500:
-                        failure("Network problem. Please try again.")
+                        failure(.networkProblem)
                     default:
-                        failure("Network problem. Please try again.")
+                        failure(.networkProblem)
                     }
                 }
             } else {
-                let banner = FloatingNotificationBanner(title: "No Internet!", subtitle: "Please make sure you are connected to the internet. Thank you!", titleFont: .SFUIText(.medium, size: 15), subtitleFont: .SFUIText(.regular, size: 15), style: .warning)
-                banner.show()
                 DLog("No Internet.")
-                failure("No Internet!.")
+                failure(.noInternet)
             }
+        }
+    }
+}
+
+enum HttpURL: String {
+    case ARTICLE_LIST           = "article/get_articles_list"
+    
+    private var BASE_URL: String {
+        return "https://www.apphusetreach.no/application/119267/"
+    }
+    
+    var url: String {
+        switch self {
+        case .ARTICLE_LIST:
+            return BASE_URL + rawValue
         }
     }
 }
